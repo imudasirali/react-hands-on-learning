385b75344b5ef8e31822e1fcbeebe936b649157e
Mudasir Ali <mudasir.ali@axelerant.com>
2024-10-29 21:08:14 +0530

    LDT-1106: useeffect exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-function App() {
+import { useState } from "react";
+import { Child } from "./Child";
 
-  return <h1>Hi</h1>
+export default function App() {
+  const [show, setShow] = useState(true);
+
+  const childComponent = show ? <Child /> : null;
+
+  return (
+    <div>
+      <button onClick={() => setShow((currentShow) => !currentShow)}>
+        Show/Hide
+      </button>
+      {childComponent}
+    </div>
+  );
 }
-
-export default App
